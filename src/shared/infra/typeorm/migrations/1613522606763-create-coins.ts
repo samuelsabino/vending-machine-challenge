ea7470c5c1bba297d135b4ec5012b6edd960baa3
Samuel <lima.samuel81@gmail.com>
2021-02-17 01:51:02 -0300

    feat: add typeorm migrations

---
+++ src/shared/infra/typeorm/migrations/1613522606763-create-coins.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateCoin1613522606763 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'coins',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '20',
+            isUnique: true
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            precision: 3,
+            scale: 2
+          },
+          {
+            name: 'quantity',
+            type: 'int'
+          },
+          {
+            name: 'created',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'version',
+            type: 'int',
+            default: 1
+          }
+        ]
+      }), true)
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('coins')
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1613536972060-coin-seed.ts
@@ -0,0 +1,16 @@
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
+
+import { Coin } from '@modules/coins/infra/typeorm/entities/coin.model'
+
+import { coinSeed } from '../seeds/coin.seed'
+
+export class CoinSeed1613536972060 implements MigrationInterface {
+  public async up (_queryRunner: QueryRunner): Promise<void> {
+    const coins = getRepository(Coin)
+
+    await coins.save(coinSeed)
+  }
+
+  public async down (_queryRunner: QueryRunner): Promise<void> {
+  }
+}
