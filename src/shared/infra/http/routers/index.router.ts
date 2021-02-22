bf3f64bf24663a4453b7985890c90b0a71ebc297
Samuel <lima.samuel81@gmail.com>
2021-02-21 23:55:33 -0300

    feat: add coin router

--- src/shared/infra/http/routers/index.router.ts
+++ src/shared/infra/http/routers/index.router.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 
+import { coinRouter } from '../../../../modules/coins/infra/http/routers/coin.router'
+
 const router = express.Router()
+
+router.use('/coins', coinRouter)
 
 export { router }
