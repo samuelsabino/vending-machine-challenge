45096d1749d6e99dd635447192c9dc6bd2a9bdc6
Samuel <lima.samuel81@gmail.com>
2021-04-07 20:51:47 -0300

    refactor(coin): add coin dto

--- src/modules/coins/dtos/coin.dto.ts
+++ src/modules/coins/dtos/coin.dto.ts
@@ -1,6 +1,10 @@
-interface ICoinToAddDTO {
+interface ICoinDTO {
+  id?: number
   value: number
   quantity: number
+  created?: Date
+  updated?: Date
+  version?: number
 }
 
-export { ICoinToAddDTO }
+export { ICoinDTO }
